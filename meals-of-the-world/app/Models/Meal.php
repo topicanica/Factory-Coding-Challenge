167f024a7446bf0dd848ea7b1c7c38b917907695
topicanica <topicanica@gmail.com>
2023-06-07 16:57:21 +0200

    Added Category, Ingredient, Meal and Tag models :construction:

---
+++ meals-of-the-world/app/Models/Meal.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+use Astrotomic\Translatable\Contracts\Translatable as TranslatableContract;
+use Astrotomic\Translatable\Translatable;
+
+class Meal extends Model implements TranslatableContract
+{
+    use Translatable;
+
+    public $translatedAttributes = ['title','description'];
+
+    protected $fillable = ['title', 'slug', 'description','status'];
+}
